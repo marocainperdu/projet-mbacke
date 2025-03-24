0e165afdbd09a539c2c3f295fe806c35ae235399
Momo Kabil <mohamedmoustaphaniang@esp.sn>
2025-03-24 12:25:59 +0000

    Add custom Nginx configuration and API configuration file

---
+++ front-end/src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiEndpoint: "https://appwrite.momokabil.duckdns.org/v1",
+    projectId: "67cd9f540022aae0f0f5",
+    apiUrl: "http://192.168.1.200:3015"
+  };
+  export default config;
+  
