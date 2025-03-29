714871221f66f8289745584af278c68e820fa470
MomoKabil <mohamedmoustaphaniang73@gmail.com>
2025-03-29 22:11:29 +0000

    nginx.conf mis à jour

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     apiEndpoint: "https://appwrite.momokabil.duckdns.org/v1",
     projectId: "67cd9f540022aae0f0f5",
-    apiUrl: "http://192.168.1.200:3015"
+    apiUrl: "http://momokabil.duckdns.org:3015"
   };
   export default config;
   
