f53c41eea40e8b232892c4db9e33043a323c6d21
Momo Kabil <mohamedmoustaphaniang@esp.sn>
2025-03-29 22:17:57 +0000

    Dernière correction

--- front-end/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    apiEndpoint: "https://appwrite.momokabil.duckdns.org/v1",
-    projectId: "67cd9f540022aae0f0f5",
-    apiUrl: "http://momokabil.duckdns.org:3015"
-  };
-  export default config;
-  
